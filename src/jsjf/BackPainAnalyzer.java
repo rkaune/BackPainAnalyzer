3dd1d77c8136060efc48937cc9f598760c6cbda3
richardkaune <richardkaune@richards-mbp>
2019-07-07 09:16:42 -0400

    Commit to rkaune REPO BackPainAnalyzer

---
+++ src/jsjf/BackPainAnalyzer.java
@@ -0,0 +1,31 @@
+/*
+ * BackPainAnalyzer.java
+ *
+ * Richard Kaune T00641439
+ * COMP 2231_SW2 Assignment 4 Question 1
+ * This completes the implementation of a DecisionTree, introduced in Chapter 19. 
+*/
+package jsjf;
+
+import java.io.*;
+
+/**
+ * BackPainAnaylyzer demonstrates the use of a binary decision tree to 
+ * diagnose back pain.
+ */
+public class BackPainAnalyzer
+{
+	/**
+	 *  Asks questions of the user to diagnose a medical problem.
+	 */
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		//System.out.println("So, you're having back pain."); // default BackPainAnalyzer prompt
+                System.out.println("So, you can't decide which fries to have... "); // demonstration of another just as complex decision tree
+
+		//DecisionTree expert = new DecisionTree("/Users/richardkaune/NetBeansProjects/BackPainAnalyzer/src/input.txt"); // BackPainAnalyzer uses input.txt
+                DecisionTree expert = new DecisionTree("/Users/richardkaune/NetBeansProjects/BackPainAnalyzer/src/inputNew.txt"); // FrenchFryChooser uses inputNew.txt
+                
+		expert.evaluate();
+	}
+}
